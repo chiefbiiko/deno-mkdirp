20950b2ee86b7fc96734755496ab83c75b3d3067
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2018-12-03 18:21:23 +0100

    decreases time complexity

--- mkdirp.ts
+++ mkdirp.ts
@@ -1,26 +1,22 @@
-import { ErrorKind, FileInfo, lstat, mkdir, platform } from 'deno'
+import { ErrorKind, FileInfo, chmod, lstat, mkdir, platform } from 'deno'
 
 const PATH_SEPARATOR: string = platform.os === 'win' ? '\\' : '/'
 
 export default async function mkdirp (path: string, mode?: number) : Promise<void> {
-  const levels: string[] = path
-    .split(/\/|\\/)
-    .reduce((acc: string[], _: string, i: number, arr: string[]) : string[] => {
-      acc.push(arr.slice(0, i + 1).join(PATH_SEPARATOR))
-      return acc
-    }, [])
   for (
-    var levels_len: number = levels.length,
+    var parts: string[] = path.split(/\/|\\/),
+      parts_len: number = parts.length,
       level: string, 
       info: FileInfo, 
       i: number = 0; 
-    i < levels_len; 
+    i < parts_len; 
     i++
   ) {
-    level = levels[i]
+    level = parts.slice(0, i + 1).join(PATH_SEPARATOR)
     try {
       info = await lstat(level)
       if (!info.isDirectory()) throw Error(`${level} is not a directory`)
+      else if (mode && info.mode !== mode) await chmod(level, mode)
     } catch (err) {
       if (err.kind !== ErrorKind.NotFound) throw err
       await mkdir(level, mode)
--- test.ts
+++ test.ts
@@ -1,28 +1,42 @@
-import { args, exit, lstat, removeAll } from 'deno'
+import { test, assert, assertEqual } from 'https://deno.land/x/testing/testing.ts'
+import { ErrKind, args, lstat, removeAll, FileInfo } from 'deno'
 import mkdirp from './mkdirp'
 
 const root_dir: string = args[1] || './mkdirp_test_root_dir'
-const leaf_dir: string = `${root_dir}/levelx/levely`
+var leaf_dir: string
 
-const test_end: Function = async (code?: number) : Promise<void> => {
-  try { await removeAll(root_dir) } catch (_) {}
-  exit(code)
-}
+test('creates nested directories...', async () => {
+  leaf_dir = `${root_dir}/levelx/levely`
+  await mkdirp(leaf_dir)
+  const info: FileInfo = await lstat(leaf_dir)
+  assert(info.isDirectory(), `${leaf_dir} is not a directory`)
+})
 
-const test: Function = async () : Promise<void> => {
+test('...irregardless of path separator', async () => {
+  leaf_dir = `${root_dir}\levelx\levely`
+  await mkdirp(leaf_dir)
+  const info: FileInfo = await lstat(leaf_dir)
+  assert(info.isDirectory(), `${leaf_dir} is not a directory`)
+})
+
+test('sets mode on all levels', async () => {
+  leaf_dir = `${root_dir}/levelx`
+  const mode: number = 0o744
+  var info: FileInfo
+  await mkdirp(leaf_dir, mode)
+  info = await lstat(root_dir)
+  assertEqual(info.mode, mode, `${root_dir} does not have mode ${mode}`)
+  info = await lstat(leaf_dir)
+  assertEqual(info.mode, mode, `${leaf_dir} does not have mode ${mode}`)
+})
+
+test('fails if the input path contains a file reference', async () => {
   try {
-    await mkdirp(leaf_dir, 0o744)
-    if (!(await lstat(leaf_dir)).isDirectory()) {
-      console.error('not ok mkdirp')
-      await test_end(1)
-    } else {
-      console.log('ok mkdirp\ntest passed')
-      await test_end(0)
-    }
+    await mkdirp('./test.ts/flex.fs')
   } catch (err) {
-    console.error('test failed', err.stack)
-    await test_end(1)
+    // TODO: assert throws DenoError kind NOT A DIRECTORY
+    assert(!!err)
   }
-}
+})
 
-test()
+// TODO: await removeAll(root_dir)
